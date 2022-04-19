3cfbb4ef17a667538b0baebd62b3fd651c3242ea
Rutwik <rrbonde@wpi.edu>
2022-04-19 14:04:38 -0400

    Imported the Libraries

---
+++ main.py
@@ -0,0 +1,12 @@
+import torch
+import numpy as np
+import os
+import shutil
+import random
+import torchvision
+from PIL import Image
+from matplotlib import pyplot as plt
+
+torch.manual_seed(0)
+print('Using Pytorch version',torch.__version__)
+
