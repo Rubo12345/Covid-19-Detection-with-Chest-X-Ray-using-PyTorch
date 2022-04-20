fa55d60fe19acae4e3d38f17190f96c7fe5be843
Rutwik <rrbonde@wpi.edu>
2022-04-20 10:13:13 -0400

    Updated the Data Loader

--- main.py
+++ main.py
@@ -79,7 +79,6 @@ train_dirs = {
     'viral': 'COVID-19 Radiography Database/viral',
     'covid': 'COVID-19 Radiography Database/covid'
 }
-
 train_dataset = ChestXRayDataset(train_dirs, train_transform)
 
 test_dirs = {
@@ -87,5 +86,11 @@ test_dirs = {
     'viral': 'COVID-19 Radiography Database/test/viral',
     'covid': 'COVID-19 Radiography Database/test/covid'
 }
-
 test_dataset = ChestXRayDataset(test_dirs, test_transform)
+
+batch_size = 6
+dl_train = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+dl_test = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+print('Number of training batches', len(dl_train))
+print('Number of test batches', len(dl_test))
